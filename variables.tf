08fdc435dd06a35106790a69ee1d06ca27785a45
Nciso <encisoenrique@gmail.com>
2023-04-14 14:50:26 -0600

    chore(demo): add basic variable questions

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "question1" {
+  type        = string
+  default     = ""
+  description = "The question to ask the user"
+}
+
+variable "question2" {
+  type        = "bool"
+  default     = false
+  description = "This is a boolean question"
+}
+
+variable "question3" {
+  type        = "list(string)"
+  default     = []
+  description = "This is a list of things"
+}
